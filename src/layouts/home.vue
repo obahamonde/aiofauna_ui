dbaf7d9ad0576fc425e4f208b2a5d1b7efa63817
obahamonde <oscarmartinbahamondemunoz@gmail.com>
2023-09-24 06:33:04 -0500

    first commit

---
+++ src/layouts/home.vue
@@ -0,0 +1,6 @@
+<template>
+ <main class="flex min-h-screen flex-col items-center justify-between p-24">
+    <RouterView />
+  <FootBar />
+</main>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useTitle('Home')
+useFavicon("/favicon.png")
+</script>
+<template>
+<Landing/>
+</template>
+
+<route lang="yaml">
+  meta:
+    layout: home
+</route>
